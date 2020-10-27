221cc092c17636b9c8d547b51187333186cbddec
Miron <hamer223@o2.pl>
2020-10-27 20:55:13 +0100

    added day 1 struggle

--- up-lab-2/MainWindow.xaml.cs
+++ up-lab-2/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Activities;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.DirectX;
+using Microsoft.DirectX.DirectSound;
+using Microsoft.Win32;
 
 namespace up_lab_2
 {
@@ -20,9 +14,38 @@ namespace up_lab_2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string filePath = "sound.wav";
+        private Device dSound;
+        private SecondaryBuffer sound;
+        private BufferDescription buffDescr = new BufferDescription();
+
+        private IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
         public MainWindow()
         {
             InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            dSound = new Device(); //c# directsound new device program stopped working 
+            dSound.SetCooperativeLevel(handle, CooperativeLevel.Priority);
+
+            //buffDescr.ControlPan = true;
+            //buffDescr.ControlVolume = true;
+            //buffDescr.ControlFrequency = true;
+            //buffDescr.ControlEffects = true;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Audio Files (.wav)|*.wav";
+
+            if (dialog.ShowDialog() == true)
+            {
+                filePath = dialog.FileName;
+            }
         }
     }
 }
